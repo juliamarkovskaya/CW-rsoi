f76b296f74e702aff2d6844af05ebbb5dbdef6cf
uliamarkovskaa <jlmarkovskaya@yandex.ru>
2023-04-22 10:57:49 +0300

    test

---
+++ src/main/java/com/cwrsoi/CwRsoiApplication.java
@@ -0,0 +1,13 @@
+package com.cwrsoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwRsoiApplication {
+;
+    public static void main(String[] args) {
+        SpringApplication.run(CwRsoiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cwrsoi/CwRsoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwrsoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwRsoiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
